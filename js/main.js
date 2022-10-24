d47981a0a148a21aa5ffe26a8fe682adfb24f745
robpap96 <robertopapagni1996@outlook.it>
2022-10-24 17:20:37 +0200

    finito

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 "use strict";
 
+const container = document.getElementById("tabellone");
+
 //stampo 100 numeri col for
-for(let i=1; i<=100; i++) {
-    if (i % 3 === 0 || i % 5 === 0){
+for(let i = 1; i<=100; i++) {
+    if (i % 3 == 0 || i % 5 == 0){
         //aggiungo condizione fizzbuzz
-        if (i % 3 === 0 && i % 5 === 0) {
-            
+        if (i % 3 == 0 && i % 5 == 0) {
+            const element = document.createElement('div');
+            element.classList.add('fizzbuzz-square');
+            element.innerHTML = i;
+            container.append(element);
         }
         //aggiungo condizione fizz
-        if (i % 3 === 0) {
-
+        if (i % 3 == 0) {
+            const element = document.createElement('div');
+            element.classList.add('fizz-square');
+            element.innerHTML = i;
+            container.append(element);
         }
         //aggiungo condizione buzz
-        if (i % 5 === 0) {
-
+        if (i % 5 == 0) {
+            const element = document.createElement('div');
+            element.classList.add('buzz-square');
+            element.innerHTML = i;
+            container.append(element);
         }
     } else {
-    //altrimenti stampo numero normale
-
-
+        //altrimenti stampo numero normale
+        const element = document.createElement('div');
+        element.classList.add('standard-square');
+        element.innerHTML = i;
+        container.append(element);
     }    
     
 }
