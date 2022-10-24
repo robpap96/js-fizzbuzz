7bc4db8007f63a4b6fd01e07264fdbf48863945c
robpap96 <robertopapagni1996@outlook.it>
2022-10-24 16:07:03 +0200

    aggiunto index, style e js con struttura algoritmo

---
+++ js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//stampo 100 numeri col for
+for(let i=1; i<=100; i++) {
+    if (i % 3 === 0 || i % 5 === 0){
+        //aggiungo condizione fizzbuzz
+        if (i % 3 === 0 && i % 5 === 0) {
+            
+        }
+        //aggiungo condizione fizz
+        if (i % 3 === 0) {
+
+        }
+        //aggiungo condizione buzz
+        if (i % 5 === 0) {
+
+        }
+    } else {
+    //altrimenti stampo numero normale
+
+
+    }    
+    
+}
